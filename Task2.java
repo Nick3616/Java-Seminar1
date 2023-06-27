ca0c08e6f3133a56ebe051b7d1e5cd860fa997b9
Stas <www.stas1996@mail.ru>
2023-06-27 18:10:28 +0300

    Задача 2

---
+++ Task2.java
@@ -0,0 +1,14 @@
+// 2) Вывести все простые числа от 1 до 1000
+
+public class Task2 {
+    public static void main(String[] args) {
+        task2();
+    }
+
+    static void task2(){
+        int max = 1000;
+        for(int i = 1; i <= max; i++){
+            System.out.println(i);
+        }
+    }
+}
